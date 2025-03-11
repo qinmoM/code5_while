5f6cc5ce83d8b4e4d023ee8bcd4dccf98584f73e
qinmoM <ma200511@qq.com>
2025-03-11 22:17:18 +0800

    code_while

---
+++ while.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=0,i=1;
+	printf("请输入不大于60的数\n");
+	while (a < 60)
+	{
+		printf("第%d次\n", i);
+		i=i+1;
+		scanf_s("%d", &a);
+	}
+	printf("完成");
+	return 0;
+}
